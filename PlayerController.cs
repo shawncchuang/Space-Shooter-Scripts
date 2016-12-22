2e6e8565cd654ac129cc34e0e4ad66c362755972
Shawn Huang <shawncc.huang@gmail.com>
2016-12-22 17:23:17 +0800

    feat: space shooter synced

---
+++ PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+private Rigidbody rb;
+	public float speed = 0.0f;
+	public float tilt;
+	public Boundary boundry;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+
+	void Update ()
+	{
+		
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position,shotSpawn.rotation);
+		}
+	}
+
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		rb = GetComponent<Rigidbody> ();
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+
+
+		rb.position = new Vector3 (
+			Mathf.Clamp (rb.position.x , boundry.xMin, boundry.xMax),
+			0.0f,
+			Mathf.Clamp (rb.position.z, boundry.zMin, boundry.zMax)
+		);
+
+		 
+
+ 
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
